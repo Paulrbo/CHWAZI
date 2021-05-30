9b0500bb494cf0696c66a08fc66848dd11fc5705
Paulrbo <72742692+Paulrbo@users.noreply.github.com>
2021-05-30 19:26:19 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,39 @@
+const Discord = require('discord.js');
+
+const client = new Discord.Client();
+
+var inGame = false;
+
+client.once('ready', () => {
+    console.log('Ready !')
+    client.user.setActivity("!chwazi element1 element2 element3");
+});
+
+client.on('message', (message) => {
+    var myArray = [''];
+    var tailleNewArray;
+    
+    //console.log(tailleArray);
+    
+    /*if (message.content.startsWith("!chwazi ")){
+        var tailleArray = myArray.push();
+        myNewArray = myData.split(' '); 
+    }*/
+    
+    if (message.content.split(" ")[0] === `!chwazi`) {
+        myArray = message.content.split(" ");
+        tailleArray = myArray.push(myArray);
+        myArray.splice(0, 1);
+    }
+    
+    function random(min, max) {
+        min = Math.ceil(0);
+        max = Math.floor(tailleArray-1);
+        randnum = Math.floor(Math.random() * (max - min +1)) + min;
+        console.log(myArray[randnum]);
+        message.reply(myArray[randnum]);
+      }
+        random(0, tailleArray);
+});
+
+client.login(process.env.TOKEN);
